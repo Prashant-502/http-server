1d95f2f696a9661bcdceb9276b7c9f978af6efd8
prashant.shigvan <prashant.shigvan@teknopoint.in>
2022-06-02 12:48:39 +0530

    server change to h2

--- index.js
+++ index.js
@@ -69,17 +69,13 @@ app.listen(8000, function () {
 });
 */
 
-// GZIP WITH HTTP2
-var path = require("path");
+// GZIP WITH HTTP
+/*var path = require("path");
 const express = require("express");
 const compression = require("compression");
 const app = express();
 app.use(compression());
 const port = process.env.PORT || 3000;
-// const spdy = require("spdy");
-// const fs = require("fs");
-// const { promisify } = require("util");
-// const readFile = promisify(fs.readFile);
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", function (req, res) {
   res.send("pong");
@@ -88,17 +84,43 @@ app.get("/", function (req, res) {
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-// spdy
-//   .createServer(
-//     {
-//       key: fs.readFileSync("./server.key"),
-//       cert: fs.readFileSync("./server.crt"),
-//     },
-//     app
-//   )
 app.listen(port, function (err) {
   if (err) {
     throw new Error(err);
   }
   console.log("Listening on port http://localhost:" + port);
+});*/
+
+// GZIP WITH HTTP2
+var path = require("path");
+const express = require("express");
+const compression = require("compression");
+const app = express();
+const port = process.env.PORT || 3000;
+const spdy = require("spdy");
+const fs = require("fs");
+// const { promisify } = require("util");
+// const readFile = promisify(fs.readFile);
+app.use(compression());
+app.use(express.static(path.join(__dirname, "public")));
+// app.get("/", function (req, res) {
+//   res.send("pong");
+// });
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+spdy
+  .createServer(
+    {
+      key: fs.readFileSync("./server.key"),
+      cert: fs.readFileSync("./server.crt"),
+    },
+    app
+  )
+  .listen(port, function (err) {
+    if (err) {
+      throw new Error(err);
+    }
+    console.log("Listening on port https://localhost:" + port);
+  });
