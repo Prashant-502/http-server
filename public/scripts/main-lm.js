36951bc0ff472d82be58ca4e28e85dab66cf2ff4
prashant.shigvan <prashant.shigvan@teknopoint.in>
2022-06-14 20:18:19 +0530

    cookie removed

--- public/scripts/main-lm.js
+++ public/scripts/main-lm.js
@@ -136,20 +136,6 @@ $(function () {
 });
 
 $(document).ready(function () {
-  function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-
-    var expires = "expires=" + d.toUTCString();
-
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";domain=.example.com;path=/";
-
-    console.log(document.cookie);
-  }
-
-  setCookie("yuvraj", "jaiswal", 30);
-
   BrowserDetect.init();
   $("body").addClass(
     BrowserDetect.browser + " " + BrowserDetect.browser + BrowserDetect.version
